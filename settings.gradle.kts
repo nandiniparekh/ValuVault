bc02bbf511768f0285f10da52138af65caec95a6
KendrickSchellenberg <kendrick.m.schellenberg@gmail.com>
2023-10-26 22:13:36 -0600

    Android structure

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT 301 Group Project"
+include(":app")
+ 
