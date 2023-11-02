bedeb25639669e971d6a691efb611113013ebcd4
“Noor <noorkalra29@gmail.com>
2023-11-02 13:36:46 -0600

    App settings fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.cmput301groupproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cmput301groupproject"
