6a4dbca7b1adaf810593a3c299e3e42d772c6018
dh-0218 <betty.hwang11@gmail.com>
2023-11-08 11:59:28 -0700

    Gallary

--- app/src/main/java/com/example/cmput301groupproject/MainActivity.java
+++ app/src/main/java/com/example/cmput301groupproject/MainActivity.java
@@ -1,53 +1,102 @@
 package com.example.cmput301groupproject;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cmput301groupproject.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TextView textView;
+    Button multiselect;
+    ArrayList<Uri> uriArrayList = new ArrayList<>();
+    RecyclerAdapter adapter;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    private static final int REQUEST_CODE = 22;
-    Button buttonTakePic;
-    ImageView imageView;
-    ActivityResultLauncher<Intent> activityResultLauncher;
 
+    //private static final int REQUEST_CODE = 22;
+    //Button buttonTakePic;
+    //ImageView imageView;
+    //ActivityResultLauncher<Intent> activityResultLauncher;
+
+    private static final int Read_Permissions = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
-        buttonTakePic = findViewById(R.id.buttonTakePic);
-        imageView = findViewById(R.id.myImage);
 
-        buttonTakePic.setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.photos);
+        recyclerView = findViewById(R.id.recyclerView_Gallery_Images);
+        multiselect = findViewById(R.id.multiselect);
+
+        adapter = new RecyclerAdapter(uriArrayList);
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 4));
+        recyclerView.setAdapter(adapter);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            !=PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Read_Permissions);
+        }
+
+        multiselect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                activityResultLauncher.launch(cameraIntent);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                }
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+
             }
         });
+    }
 
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && requestCode == Activity.RESULT_OK){
+            if(data.getClipData() != null){
+                int x = data.getClipData().getItemCount();
+
+                for(int i=0; i<x; i++){
+                    uriArrayList.add(data.getClipData().getItemAt(i).getUri());
+                }
+                adapter.notifyDataSetChanged();
+                textView.setText("Photos ("+uriArrayList.size()+")");
+
+            }else if(data.getData() != null){
+                String imageURL = data.getData().getPath();
+                uriArrayList.add(Uri.parse(imageURL));
             }
         }
     }
 }
+
 
