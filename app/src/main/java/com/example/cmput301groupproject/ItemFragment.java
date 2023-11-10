662e9ef3c7d11fa38694b658cf0820df6b743abc
Jaskeerat <keerat00@gmail.com>
2023-11-09 18:57:04 -0700

    Committing for issue US 05.02.01. Barcode Scanning complete, number recognition to be added.

--- app/src/main/java/com/example/cmput301groupproject/ItemFragment.java
+++ app/src/main/java/com/example/cmput301groupproject/ItemFragment.java
@@ -1,17 +1,33 @@
 package com.example.cmput301groupproject;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
+
 
 public class ItemFragment extends DialogFragment {
     private String titleDesc = "Add Item";
@@ -21,11 +37,18 @@ public class ItemFragment extends DialogFragment {
     private EditText serialNumber;
     private EditText estimatedValue;
     private EditText comment;
-
     private EditText purchaseDate;
-
+    private Button scanBarcodeButton;
+    private FirebaseFirestore db;
+    private CollectionReference itemsRef;
+    private GmsBarcodeScannerOptions options = new GmsBarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                    Barcode.FORMAT_UPC_A)
+            .enableAutoZoom()
+            .build();
     private HouseholdItem passedHouseholdItem;
     private OnFragmentInteractionListener listener;
+
 
     public interface OnFragmentInteractionListener {
         void onHouseholdItemAdded(HouseholdItem newItem);
@@ -58,6 +81,10 @@ public class ItemFragment extends DialogFragment {
         comment = view.findViewById(R.id.comment_edit_text);
         purchaseDate = view.findViewById(R.id.purchase_date_edit_text);
 
+        scanBarcodeButton = view.findViewById(R.id.scan_barcode_button);
+        scanBarcodeButton.setOnClickListener(view1 -> startScanner());
+        scanBarcodeButton = view.findViewById(R.id.scan_barcode_button);
+        scanBarcodeButton.setOnClickListener(view1 -> startScanner());
         Bundle args = getArguments();
         if (args != null) {
             titleDesc = "Edit Item";
@@ -119,6 +146,41 @@ public class ItemFragment extends DialogFragment {
         return builder.create();
     }
 
+    private void startScanner(){
+        GmsBarcodeScanner scanner = GmsBarcodeScanning.getClient(getContext(),options);
+
+        // The task failed with an exception
+        scanner
+                .startScan()
+                .addOnSuccessListener(
+                        barcode -> {
+                            String scannedBarcode = barcode.getRawValue();
+                            DocumentReference docRef = db.collection("Items_Barcode_info").document(scannedBarcode);
+                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document = task.getResult();
+                                        if (document.exists()) {
+                                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                            description.setText((String)document.get("Product Description"));
+                                        } else {
+                                            Log.d(TAG, "No such item exists in the database");
+                                        }
+                                    } else {
+                                        Log.d(TAG, "get failed with ", task.getException());
+                                    }
+                                }
+                            });
+                        })
+                .addOnCanceledListener(
+                        () -> {
+                            // The task has been cancelled
+                        })
+                .addOnFailureListener(
+                        Throwable::getMessage);
+
+    }
     public static ItemFragment newInstance(HouseholdItem item) {
         Bundle args = new Bundle();
         args.putSerializable("item", item);
