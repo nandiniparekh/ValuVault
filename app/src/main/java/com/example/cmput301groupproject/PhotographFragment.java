3832f80c5b8221f9b44fc14e6e6f7f5ebb8b1d41
dh-0218 <betty.hwang11@gmail.com>
2023-11-26 16:36:42 -0700

    photos

--- app/src/main/java/com/example/cmput301groupproject/PhotographFragment.java
+++ app/src/main/java/com/example/cmput301groupproject/PhotographFragment.java
@@ -1,5 +1,6 @@
 package com.example.cmput301groupproject;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -20,20 +21,26 @@ import androidx.fragment.app.Fragment;
 
 public class PhotographFragment extends Fragment {
 
+    private Button attachButton;
+    private Button deleteButton;
     private Button btnPicture;
     private ImageView imageView;
     private ActivityResultLauncher<Intent> activityResultLauncher;
 
 
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photograph, container, false);
 
+        attachButton = view.findViewById(R.id.attach_button);
+        deleteButton = view.findViewById(R.id.delete_button);
         btnPicture = view.findViewById(R.id.btncamera);
         imageView = view.findViewById(R.id.image);
 
         btnPicture.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -53,5 +60,6 @@ public class PhotographFragment extends Fragment {
         });
 
         return view;
+
     }
 }
